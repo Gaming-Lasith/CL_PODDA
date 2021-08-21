f3fd16fa38c7f1e1599ded230208b4d28aeeccc2
CL _ PODDA <88972255+Dropmll@users.noreply.github.com>
2021-08-21 21:14:59 +0530

    Create index.js

---
+++ CL_PODDA/index.js
@@ -0,0 +1,14 @@
+/* 2021 PODDA.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+PODDA
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
